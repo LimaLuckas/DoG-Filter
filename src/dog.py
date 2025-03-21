1d6dce250a06427322e4c02265564bfdfee1b0a0
LimaLuckas <lfreire05@gmail.com>
2025-03-20 21:21:10 -0300

    Addition of another image in examples

--- src/dog.py
+++ src/dog.py
@@ -1,7 +1,6 @@
 import cv2 as cv
 import numpy as np
 
-
 class ExtendeDoGFilter:
     def __init__(self, image: str, sigma: float = 1, k: float = 1., p: float =1., phi: float = 0.1, e: float = 0.1) -> None:
         self.sigma = sigma
@@ -10,6 +9,7 @@ class ExtendeDoGFilter:
         self.phi = phi
         self.e = e
         self.image_array = cv.imread(image)
+        assert self.image_array is not None, "Image not found"
         self.image_array = self.image_array.astype(np.float32) / 255.0
 
     def set_image_array(self, pathname: str) -> np.ndarray:
@@ -18,7 +18,8 @@ class ExtendeDoGFilter:
         Set the image_array to a new image_array if needed.
         """
         #read the image_array as a grayscale image_array
-        self.image_array = cv.imread(pathname, cv.IMREAD_COLOR_RGB)
+        self.image_array = cv.imread(pathname)
+        assert self.image_array is not None, "Image not found"
         self.image_array = self.image_array.astype(np.float32) / 255.0
 
     def dog(self) -> np.ndarray:
@@ -27,7 +28,7 @@ class ExtendeDoGFilter:
         Output: Difference of Gaussians applied on a numpy array.\n
         Computes the reparametrized Difference of Gaussians on a grayscale image array
         """ 
-        gray_image = cv.cvtColor(self.image_array, cv.COLOR_RGB2GRAY)
+        gray_image = cv.cvtColor(self.image_array, cv.COLOR_BGR2GRAY)
         #set trackbar sliders values
         # sigma is the standard deviation value of the blurs, 0.01 is the minimum value of sigma to avoid ksize = 0
         self.sigma = max(cv.getTrackbarPos("Sigma", "XDoG") / 100, 0.01)
@@ -57,7 +58,7 @@ class ExtendeDoGFilter:
         self.e = cv.getTrackbarPos("Epsilon", "XDoG") / 100
 
         #use different functions for the thresholding, e.g. sigmoid, ReLU, exponential, etc. Be creative!
-        result = np.where(dog_array >= self.e, 1, 1/np.pi * np.arctan(self.phi * (dog_array - self.e)) + 0.5)
+        result = np.where(dog_array >= self.e, 1, 1 + np.tanh(self.phi * (dog_array - self.e)))
         
         if masking:
 
